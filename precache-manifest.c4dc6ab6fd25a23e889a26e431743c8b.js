88de4f8f76877b4c68e0685359b7e7fde310df8a
natalliasdh <natallia.sdh@gmail.com>
2019-06-18 17:01:38 -0400

    Updates

---
+++ precache-manifest.c4dc6ab6fd25a23e889a26e431743c8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1784c8ac7de74509cfcb58e020b33f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b66a0d71d35d9258f31",
+    "url": "/static/css/main.9a06768c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "2b66a0d71d35d9258f31",
+    "url": "/static/js/main.006b8dab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
